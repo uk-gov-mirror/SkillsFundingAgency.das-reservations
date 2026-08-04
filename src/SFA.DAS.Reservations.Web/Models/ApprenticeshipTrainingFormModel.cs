7f3d9af584e19e21aa9c04ab02a1ceca69365599
IOANNIDOU, Stefania <Stefania.IOANNIDOU@education.gov.uk>
2026-08-04 14:27:42 +0100

    APPMAN-2592 - Updated error message wording

--- src/SFA.DAS.Reservations.Web/Models/ApprenticeshipTrainingFormModel.cs
+++ src/SFA.DAS.Reservations.Web/Models/ApprenticeshipTrainingFormModel.cs
@@ -6,7 +6,7 @@ namespace SFA.DAS.Reservations.Web.Models
     {
         [Required(ErrorMessage = "You must select a start date")]
         public string StartDate { get; set; }
-        [Required(ErrorMessage = "Select which apprenticeship training your apprentice will take")]
+        [Required(ErrorMessage = "Select which training your learner will take")]
         public string SelectedCourseId { get; set; }
         public string AccountLegalEntityPublicHashedId { get; set; }
         public string CohortRef { get; set; }
